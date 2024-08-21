f2b8d52977515fafea188d58316211ac65007da9
CLEITIN6966 <cleitinholeiti@gmail.com>
2024-08-21 14:09:46 -0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -1,3 +1,1 @@
-for i = 1, 100 do
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/a-cada-segundo-vc-recebe-1-de-velocidade-de-caminhada-script/main/.lua"))()
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CLEITIN6966/a-cada-segundo-vc-recebe-1-de-velocidade-de-caminhada-script/main/.lua"))()
